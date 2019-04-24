3773c6ecdde6268d9fe4304139e5b49ae3eab5b0
ravinder85 <srreddy2003@gmail.com>
2019-04-24 10:19:55 +0200

    Delete DiffRendererService.java

--- src/main/java/xyz/foobar/DiffRendererService.java
+++
@@ -1,9 +0,0 @@
-package xyz.foobar;
-
-public class DiffRendererService implements DiffRenderer {
-    @Override
-    public String render(Diff<?> diff) throws DiffException {
-        System.out.println("diff$$$$"+diff.getParent());
-        return null;
-    }
-}
